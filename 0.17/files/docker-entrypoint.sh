708b94c89b201da4b2556d37fc9ddabc0cb74c96
Jon Fairbanks <fairplay89@gmail.com>
2019-04-24 00:48:34 -0700

    Support PUID/PGID

--- 0.17/files/docker-entrypoint.sh
+++ 0.17/files/docker-entrypoint.sh
@@ -35,6 +35,9 @@ if find -L $SAVES -iname \*.tmp.zip -mindepth 1 -print | grep -q .; then
 fi
 
 if [ "$(id -u)" = '0' ]; then
+  # Update the User and Group ID based on the PUID/PGID variables
+  usermod -o -u $PUID factorio
+  groupmod -o -g $PGID factorio
   # Take ownership of factorio data if running as root
   chown -R factorio:factorio $FACTORIO_VOL
   # Make sure we own temp
